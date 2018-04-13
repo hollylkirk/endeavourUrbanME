f0a3fb27036c8bb93f101cf09234a3b4e9cbdcbd
Holly Kirk <hollylkirk@gmail.com>
2018-04-13 14:00:43 +1000

    creating a fake raster of urban matrix to test model, and then a matrix of distances from the park

--- analysis/prelimModelling.R
+++ analysis/prelimModelling.R
@@ -1,12 +1,12 @@
 
-#  
+# Testing out the basic movement model, ready to run on real world raster of CoM
 # simulate a movement-based abundance distribution map
 
 set.seed(1)
 library (raster)
 
 
-# make a fake raster of habitat type
+# make a fake raster of habitat type (park, housing matrix and industrial matrix)
 # three habitats: parks, housing, and industrial
 # stipulate number of columns and rows (e.g. 30)
 nrow <- ncol <- 30
@@ -20,4 +20,17 @@ mat[seq_len(nrow / 2), -1] <- 2
 mat[ceiling(nrow / 2) + seq_len(nrow / 2), -1] <- 3 
 # make this into a raster object class 1000 by 1000 cells
 ras <- raster::raster(mat, 0, 1000, 0, 1000)
+
+
+# calculate raster of distance from the park
+# duplicate the raster from above
+mask <- ras
+# get rid of cells that are classed as park
+mask[mask != 1] <- NA
+# use distance function from raster package (in raster units or pixels)
+dist <- raster::distance(mask)
+
+
+
+
 
