2b1422492765183324521de7bb32a31a2d21aa56
Holly Kirk <hollylkirk@gmail.com>
2019-02-01 14:48:15 +1100

    Provides additional comments and thoughts

--- 00_prelimModelling.R
+++ 00_prelimModelling.R
@@ -1,9 +1,12 @@
-
+rm(list = ls())
 # Testing out the basic movement model, ready to run on real world raster of CoM
-# simulate a movement-based abundance distribution map
+# simulate a movement-based abundance distribution map.
+# Thoughts on how to test this hypothesis
 
 set.seed(1)
 library (raster)
+library(devtools)
+# library(ochRe)
 
 
 # make a fake raster of habitat type (park, housing matrix and industrial matrix)
@@ -14,12 +17,11 @@ nrow <- ncol <- 30
 mat <- matrix(0, nrow, ncol)
 # first column is the border of the park
 mat[, 1] <- 1
-# second set of columns and rows is urban matrix type housing
-mat[seq_len(nrow / 2), -1] <- 2  
-# second set of columns and rows is urban matrix type industrial
-mat[ceiling(nrow / 2) + seq_len(nrow / 2), -1] <- 3 
+# rest of columns are non-park
+mat[ , -1] <- 2  
 # make this into a raster object class 1000 by 1000 cells
 ras <- raster::raster(mat, 0, 1000, 0, 1000)
+
 
 
 # calculate raster of distance from the park
@@ -31,32 +33,18 @@ mask[mask != 1] <- NA
 dist <- raster::distance(mask)
 
 
-# movement ability differs depending on class of landscape
+# movement ability differs depending on bird species
 # park can move anywhere,
-# housing harder to move, industrial even more difficult
-movement_ability <- list(park = Inf,
-                         housing = 500,
-                         industrial = 100)
+movement_ability <- 300
 
 
 
 # define function to calculate relative probability of moving to a pixel from
 # the park. This is the basic movement model definition
-p_move <- function (distance, habitat, ...) {
-  # if habitat is type 2, movement ability is 500
-  # if habitat is something other than 2, movement ability is 100
-  mean_movement <- ifelse(habitat == 2,
-                          movement_ability$housing,
-                          movement_ability$industrial)
-  # is habitat is type 0, movement ability is infinite
-  mean_movement <- ifelse(habitat == 0,
-                          movement_ability$park,
-                          mean_movement)
-  # define movement model 
-  lambda <- 1 / mean_movement
+p_move <- function (distance, movement_ability) {
+  lambda <- 1 / movement_ability
   exp(-lambda * distance)
 }
-
 
 
 
@@ -65,24 +53,22 @@ p_move <- function (distance, habitat, ...) {
 # make a bunch of distances from park
 dists <- seq(0, 1000, length.out = 100)
 # apply p_move to range of distances, where habitat = 2
-Pmove_housing <- vapply(dists, p_move, habitat = 2, FUN.VALUE = 1)
-# apply p_move to range of distances, where habitat = 3
-Pmove_industrial <- vapply(dists, p_move, habitat = 3, FUN.VALUE = 1)
-# plot the two probability curves
-plot(Pmove_housing ~ dists, type = "l", ylim = c(0, 1))
-lines(Pmove_industrial ~ dists, lty = 2)
+Pmove <- p_move(dists, movement_ability)
+# plot the probability curves
+plot(Pmove ~ dists, type = "l", ylim = c(0, 1))
 
 # function to make a raster heatmap of relative dispersal probabiltiy
 p_move_raster <- function (x, ...) {
-  p_move(x[, 1], x[, 2])
+  p_move(x, movement_ability)
 }
 # create raster heatmap of movement probability
-prob_raster <- raster::calc(stack(dist, ras),
+prob <- raster::calc(dist,
                      p_move_raster)
 
-
+plot(prob)
 
 # ----------------------------------
+
 
 # turn into expected number of individuals
 expected_count <- prob * rpois(1, 10)
@@ -96,34 +82,67 @@ locs <- raster::xyFromCell(expected_count, expected[, "cell"])
 counts <- rpois(nrow(expected), expected[, 2])
 
 # plot the landscape
-plot(expected_count, col = colorRampPalette(c("light green", "seagreen"))(100))
+pal <- colorRampPalette(ochre_palettes[["olsen_seq"]])
+plot(expected_count, col = pal(100))
 points(locs, cex = log(counts + 1.2), pch = 21, bg = "light blue")
 
-# how to fit the model and estimate the parameters from count data
+
+
+
+# how to fit the model and estimate the parameters from count data?
 
 # extract the data
+# create dataframe of observations of bird numbers
 df <- data.frame(count = counts)
+# add column of "distance from park" for each sample location
 df$distance <- extract(dist, locs)
-df$land_cover <- extract(ras, locs)
-df$land_cover <- names(movement_ability)[df$land_cover]
 
 # fit models to estimate movement abilities in different habitat types
-m_housing <- glm(count ~ distance,
-                 family = poisson,
-                 data = df[df$land_cover == "housing", ])
-beta_housing <- coef(m_housing)[2]
-mean_movement_housing <- 1 / -beta_housing
-
-m_industrial <- glm(count ~ distance,
+model_basic <- glm(count ~ distance,
                     family = poisson,
-                    data = df[df$land_cover == "industrial", ])
-beta_industrial <- coef(m_industrial)[2]
-mean_movement_industrial <- 1 / -beta_industrial
+                    data = df)
+
+# get second coefficient (slope of line)
+beta <- coef(model_basic)[2]
+# convert the coefficient to the mean movement ability (flight distance)
+mean_movement <- 1 / -beta
+
+
 
 # comapre them to the true values
-unlist(movement_ability[-1])
-c(mean_movement_housing, mean_movement_industrial)
+movement_ability
+
+# print different model outcomes into a vector
+c(mean_movement)
 
 
+# statistically test whether the null hypothesis model (based on rural movement distaance estimates)
+# is different to the alternative hypothesis model, based on observed data.
 
+# create a log offset (fixed slope) based on rural literature values
+# convert the coefficient to the mean movement ability (flight distance)
+# mean_movement <- 1 / -beta
+# 1 / mean_movement <- -beta
+
+# set the mean move distance for this species
+mean_movement_null <- 200
+# calculate the beta (slope on the line) for this species
+beta_null <- -1 / mean_movement_null
+# calculate the log of the expected relative abundance for each location for this species
+null_logExpected <- beta_null * df$distance
+
+
+# null hypothesis model
+# offset term in model because we are using a known coefficent rather than an estimated one
+model_null <- glm(count ~ offset(null_logExpected),
+                  family = poisson,
+                  data = df)
+
+
+# likelihood ratio test to compare the two different models (null and observed_basic)
+# just use ANOVA as this is the same thing when used on GLMs
+t <- anova(model_null, model_basic, test = "Chisq")
+t
+
+# LLR test compares how well each model fits the data. And also how complex the model is
 
